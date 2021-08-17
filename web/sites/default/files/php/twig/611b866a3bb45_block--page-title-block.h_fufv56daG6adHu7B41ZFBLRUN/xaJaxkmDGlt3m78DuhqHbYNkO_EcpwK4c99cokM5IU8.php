2f1010453b46376143cc5c8d87fb8a25b38a7479
t-ores <x-da@tutanota.com>
2021-08-17 14:53:01 +0300

    first commit

---
+++ web/sites/default/files/php/twig/611b866a3bb45_block--page-title-block.h_fufv56daG6adHu7B41ZFBLRUN/xaJaxkmDGlt3m78DuhqHbYNkO_EcpwK4c99cokM5IU8.php
@@ -0,0 +1,93 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/drupalbook/templates/block/block--page-title-block.html.twig */
+class __TwigTemplate_11c1ab154a8dc8835787948b8bd37ea91cc837a36237331dc76a574aa1b594b8 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 9
+        return "block--bare.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("block--bare.html.twig", "themes/drupalbook/templates/block/block--page-title-block.html.twig", 9);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/drupalbook/templates/block/block--page-title-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  36 => 9,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/drupalbook/templates/block/block--page-title-block.html.twig", "/var/www/drupal.local/web/themes/drupalbook/templates/block/block--page-title-block.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
